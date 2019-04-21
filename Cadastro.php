6d5d1966e10e78256973393c60ee498a2e0ca0c4
Luiz Vitor <47873430+Luizfapi5229@users.noreply.github.com>
2019-04-21 18:40:12 -0300

    Add files via upload

---
+++ Cadastro.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="vaitomanocutodomundodesgraca.css" >
+		<title>Cadastro</title>
+	</head>
+	
+	
+	<body>
+			<div id="cabecalho">
+			
+			<img id="img1" src = "logo.png">
+			</div>
+			
+		<div id="menu">
+				 <center>
+				<ul>
+					<li><a href="Cadastro.php">Cadastro</a></li>
+					<li><a href="Login.php">Login</a></li>
+					<li><a href="Inicio.php">Inicio</a></li>
+					<li><a href="">Categoria</a></li>
+					<li><a href="">Sobre</a></li>
+				</ul>
+				</center>	
+			</div>
+			<br><br><br><br>
+			<br><br><br><br>
+			<br><br><br><br>
+			<br><br><br><br>
+			<div id="corpo">
+				<center>		 
+				<h1><font> CADASTRO </font> </h1>	
+					<table  id="cadastroBox" border = "2">
+						<td align = "center">
+							<form id = "Cadastro" name = "Cadastro.php" action="">
+							NOME <br>
+						    <input type="text" id="nomeCad" /><br><br>
+							SOBRENOME <br>
+							<input type="text" id="sobrenomeCad" /><br><br>
+							CPF <br>
+							<input type="text" id="cpfCad" /><br><br>
+							EMAIL <br>
+							<input type="text" id="emailCad" /><br><br>
+							SENHA <br>
+							<input type="text" id="senhaCad" /><br><br>
+							CONFIRMAR SENHA <br>
+						    <input type="text" id="confirmSenhaCad" /><br><br><br>
+								  
+								  <a href="#" class="myButton">Cadastrar</a><br><br>
+								  <a href="http://localhost/xesque/Login.php" class="myButton">Voltar ao Login</a><br><br>
+						</td>	
+					</table>
+				</form>
+				
+			</center>
+	
+	
+			</div>
+			
+			<div id="rodape">
+			<img id = "img2" src = "imgRodape.png" align = "left">
+			<img id="face" src="face.svg" align="right">
+			<img id="face" src="insta.svg" align="right">
+			<img id="face" src="tt.svg" align="right">
+			<img id="face" src="you.svg" align="right">
+			
+			</div>
+	
+	</body>
+
+</html>
---
+++ Login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="vaitomanocutodomundodesgraca.css" >
+		<title>Inicio</title>
+	</head>
+	
+	
+	<body>
+			<div id="cabecalho">
+			
+			<img id="img1" src = "logo.png">
+			</div>
+			
+		<div id="menu">
+				 <center>
+				<ul>
+					<li><a href="Cadastro.php">Cadastro</a></li>
+					<li><a href="Login.php">Login</a></li>
+					<li><a href="Inicio.php">Inicio</a></li>
+					<li><a href="">Categoria</a></li>
+					<li><a href="">Sobre</a></li>
+				</ul>
+				</center>	
+			</div>
+			<br><br><br><br>
+			<br><br><br><br>
+			
+			<div id="corpo">
+			<center>		 
+				<h1><font> LOGIN </font> </h1>	
+					<table  id="loginBox" border = "2">
+						<td align = "center">
+							<form id = "Login" name = "Login.php" action="">
+							LOGIN: <input type="text" id="Login" /><br><br>
+							SENHA: <input type="text" id="Senha" /><br><br><br><br>
+								  <a href="#" class="myButton">Sing in</a><br><br>
+								  <a href="http://localhost/xesque/Cadastro.php" class="myButton">Cadastrar</a><br><br>
+								  <a href="#" class="myButton">Esqueceu sua senha?</a>
+						</td>	
+					</table>
+				</form>
+				
+			</center>
+	
+	
+			</div>
+			<div id="rodape">
+			<img id = "img2" src = "imgRodape.png" align = "left">
+			<img id="face" src="face.svg" align="right">
+			<img id="face" src="insta.svg" align="right">
+			<img id="face" src="tt.svg" align="right">
+			<img id="face" src="you.svg" align="right">
+			
+			</div>
+	
+	</body>
+
+</html>
