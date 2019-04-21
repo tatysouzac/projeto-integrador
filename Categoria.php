f9e723e77fc1d529a4e7ea86c60efd57889e9b0d
tatysouzac <47873452+tatysouzac@users.noreply.github.com>
2019-04-21 19:34:22 -0300

    Add files via upload

---
+++ Categoria.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="css.css" >
+		<title>Inicio</title>
+	</head>
+	
+	
+	<body>
+			<div id="cabecalho">
+			<img id="img1" src = "img/logo.png">
+
+			</div>
+			<div id="menu">
+				<center>
+				<ul>
+					<li><a href="Inicio.php">Inicio</a></li>
+					<li><a href="">Categoria</a></li>
+					<li><a href="">Contato</a></li>
+					<li><a href="">Sobre</a></li>
+				</ul>
+				</center>
+			</div>
+			<div id="corpo">
+			<table>
+	 <tr>
+		<td>
+			&ensp;&ensp;&ensp;<img src="https://assets.adidas.com/images/w_320,h_320,f_auto,q_auto:sensitive,fl_lossy/13bad9c727474b41baf0a81800f39fb5_9366/Tenis_Adiease_Preto_BY4028_02_hover_frv.jpg" width="250" height="250">
+		</td>
+	
+		<td>
+			&ensp;&ensp;&ensp;<img src="https://assets.adidas.com/images/w_600,h_600,f_auto,q_auto:sensitive,fl_lossy/1acf45cc84f14a17b55ea919015e2104_9366/Tenis_Campus_Adv_Vermelho_B22714_01_standard.jpg" width="250" height="250">
+		</td>
+		<td>
+			&ensp;&ensp;&ensp;<img src="https://assets.adidas.com/images/w_420,h_420,f_auto,q_auto:sensitive,fl_lossy/5355c56049424a6094fca9a40121c4c6_9366/Tenis_Nmd_R1_W_Roxo_BD8012_01_standard.jpg" width="250" height="250">
+		</td>			
+		<td>
+			&ensp;&ensp;&ensp;<img src="https://assets.nike.com.br/ArquivoExibir/805883.jpg" width="250" height="250">
+		</td>
+				<td>
+			&ensp;&ensp;&ensp;<img src="https://assets.nike.com.br/ArquivoExibir/1369210.jpg" width="250" height="250">
+		</td>
+	</tr>
+		<tr>
+		<td>
+			<center><h4>Tênis Adiease<h4></center>
+
+			<h4>R$ 330,00</h4>
+
+			<center><a href="#" class="myButton">Comprar</a></center>
+		</td>
+		<td>
+			<center><h4>Tênis Campus ADV</h4></center>
+			
+			<h4><p style="margin:30px">R$ 365,20</p></h4>
+			
+			<center><a href="#" class="myButton">Comprar</a></center>
+			
+		</td>
+		<td>
+			<center><h4>Tênis NMD R1 W<h4></center>
+			
+			<h4><p style="margin:30px">R$ 240,90</p></h4>
+			
+
+			<center><a href="#" class="myButton">Comprar</a></center>
+		</td>
+		<td>
+			<center><h4>Tênis Nike SB Check Solar <h4></center>
+			
+			<h4><p style="margin:30px">R$ 279,90</p></h4>
+			
+
+			<center><a href="#" class="myButton">Comprar</a></center>
+		</td>
+        <td>
+			<center><h4>TÊNIS Nike SB Check Canvas <h4></center>
+			
+			<h4><p style="margin:30px">R$ 129,90</p></h4>
+			
+			<center><a href="#" class="myButton">Comprar</a></center>
+		</td>			
+	</tr>
+	</table>
+			
+			</div>
+			<div id="rodape">
+
+<table>
+			<tr>
+			</br>
+			</tr>
+			<tr>
+			<td>
+			<img src="https://static.netshoes.com.br/banners/l_netshoes/2018-06-12/204_nike.png" alt="">
+			</td>
+			<td>
+			<img src="https://static.netshoes.com.br/banners/l_netshoes/2018-10-10/2918_adidas.png" alt="">
+			</td>
+			<td>
+			<img src="https://static.netshoes.com.br/banners/l_netshoes/2018-11-05/1918_puma.png" alt="">
+			</td>
+			<td>
+			<img src="https://static.netshoes.com.br/banners/l_netshoes/2018-09-27/8918_mizuno.png" alt="">
+			</td>
+			<td>
+			<img src="https://static.netshoes.com.br/banners/l_netshoes/2018-06-12/2338_olimpikus.png" alt="">
+			</td>
+			<td>
+			<img src="https://static.netshoes.com.br/banners/l_netshoes/2018-06-12/188_new_balance.png" alt="">
+			</td>
+			<td>
+			<img src="https://static.netshoes.com.br/banners/l_netshoes/2018-06-12/4719_kappas.png" alt="">
+			</td>
+			<td>
+			<img src="https://static.netshoes.com.br/banners/l_netshoes/2018-06-12/1214_gonew.png" alt="">
+			</td>
+			<td>
+			<img src="https://static.netshoes.com.br/banners/l_netshoes/2018-06-12/1476_under.png" alt="">
+			</td>
+			</tr>
+			</table>
+			
+			</div>
+	
+	</body>
+
+</html>
