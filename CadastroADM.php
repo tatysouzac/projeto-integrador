d3e495ff28cf96a791abdda2ead4bcead0c56759
Luiz Vitor <47873430+Luizfapi5229@users.noreply.github.com>
2019-04-23 21:35:19 -0300

    Add files via upload

---
+++ CadastroADM.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="vaitomanocutodomundodesgraca.css" >
+		<title>Cadastro</title>
+	</head>
+	
+	
+	<body>
+			<div id="cabecalho">
+			
+			<img id="img1" src = "logo.png">
+			</div>
+			
+		<div id="menu">
+				 <center>
+				<ul>
+					<li><a href="Cadastro.php">Cadastro</a></li>
+					<li><a href="Login.php">Login</a></li>
+					<li><a href="Inicio.php">Inicio</a></li>
+					<li><a href="">Categoria</a></li>
+					<li><a href="">Sobre</a></li>
+				</ul>
+				</center>	
+			</div>
+			<br><br><br><br>
+			<br><br><br><br>
+			<br><br><br><br>
+			<br><br><br><br>
+			<div id="corpo">
+				<center>		 
+				<h1><font> CADASTRO ADM </font> </h1>	
+					<table  id="cadastroBox" border = "2">
+						<td align = "center">
+							<form id = "Cadastro" name = "Cadastro.php" action="">
+							NOME <br>
+						    <input type="text" id="nomeCadAdm" /><br><br>
+							SOBRENOME <br>
+							<input type="text" id="sobrenomeCadAdm" /><br><br>
+							CPF <br>
+							<input type="text" id="cpfCadAdm" /><br><br>
+							EMAIL <br>
+							<input type="text" id="emailCadAdm" /><br><br>
+							ID ADM <br>
+						    <input type="text" id="IdAdm" /><br><br>
+							SENHA <br>
+							<input type="text" id="senhaCadAdm" /><br><br>
+							CONFIRMAR SENHA <br>
+						    <input type="text" id="confirmSenhaCadAdm" /><br><br><br>
+								  
+								  <a href="#" class="myButton">Cadastrar</a><br><br>
+								  <a href="http://localhost/xesque/Login.php" class="myButton">Voltar ao Login</a><br><br>
+						</td>	
+					</table>
+				</form>
+				
+			</center>
+	
+	
+			</div>
+			
+			<div id="rodape">
+			<img id = "img2" src = "imgRodape.png" align = "left">
+			<img id="face" src="face.svg" align="right">
+			<img id="face" src="insta.svg" align="right">
+			<img id="face" src="tt.svg" align="right">
+			<img id="face" src="you.svg" align="right">
+			
+			</div>
+	
+	</body>
+
+</html>
---
+++ LoginADM.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="vaitomanocutodomundodesgraca.css" >
+		<title>Inicio</title>
+	</head>
+	
+	
+	<body>
+			<div id="cabecalho">
+			
+			<img id="img1" src = "logo.png">
+			</div>
+			
+		<div id="menu">
+				 <center>
+				<ul>
+					<li><a href="Cadastro.php">Cadastro</a></li>
+					<li><a href="Login.php">Login</a></li>
+					<li><a href="Inicio.php">Inicio</a></li>
+					<li><a href="">Categoria</a></li>
+					<li><a href="">Sobre</a></li>
+				</ul>
+				</center>	
+			</div>
+			<br><br><br><br>
+			<br><br><br><br>
+			
+			<div id="corpo">
+			<center>		 
+				<h1><font> LOGIN </font> </h1>	
+					<table  id="loginBox" border = "2">
+						<td align = "center">
+							<div>
+							
+								
+								<a href="#" class="myButton">Usuario</a>
+								<a href="#" class="myButton">Administrador</a>
+								<a href="#" class="myButton">Vendedor</a><br><br><br><br>
+									
+							
+							</div>	
+							<div>					
+									<form id = "Login" name = "Login.php" action="">
+									ID ADM: <input type="text" id="idAdm" /><br><br>
+									SENHA:  <input type="text" id="SenhaAdm" /><br><br><br><br>
+									<a href="#" class="myButton">Entrar</a><br><br>
+									<a href="http://localhost/xesque/Cadastro.php" class="myButton">Cadastrar</a><br><br>
+									<a href="#" class="myButton">Esqueceu sua senha?</a>
+							</div>
+						</td>	
+					</table>
+				</form>
+				
+			</center>
+	
+	
+			</div>
+			<div id="rodape">
+			<img id = "img2" src = "imgRodape.png" align = "left">
+			<img id="face" src="face.svg" align="right">
+			<img id="face" src="insta.svg" align="right">
+			<img id="face" src="tt.svg" align="right">
+			<img id="face" src="you.svg" align="right">
+			
+			</div>
+	
+	</body>
+
+</html>
